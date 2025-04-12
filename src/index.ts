d766dd630da0e5cf9ee21733642f8f6cf0b01bc5
jihyun hwang <jihyunhwang@jihyuns-MacBook-Air.local>
2025-04-12 14:57:15 +0200

    revert

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,25 @@ const cartItem1 = new CartItem({
   productId: "12345",
   productName: "test",
   quantity: new Quantity({ quantity: 1 }),
-  unitPrice: 100,
+  unitPrice: 123,
 });
 
 const cartItem2 = new CartItem({
   productId: "123",
   productName: "test",
   quantity: new Quantity({ quantity: 1 }),
-  unitPrice: 200,
+  unitPrice: 123,
 });
 
 const cart = new Cart({
   items: [cartItem1, cartItem2],
 });
 const x = cart.addCartItem(cartItem1);
+<<<<<<< HEAD
 const y = cart.clearAllItems()
 console.log("here -", y);
+=======
+const y = cart.addCartItem(cartItem1);
+const z = cart.addCartItem(cartItem1);
+console.log("here -", z);
+>>>>>>> parent of 7e9540c (Add calcualteTotalPrice)
