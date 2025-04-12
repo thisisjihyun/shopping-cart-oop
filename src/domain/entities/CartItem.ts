4414313ff957c51b8405a917ed37026cf1dfdaa6
jihyun hwang <jihyunhwang@jihyuns-MacBook-Air.local>
2025-04-12 14:59:46 +0200

    revert

---
+++ src/domain/entities/CartItem.ts
@@ -0,0 +1,39 @@
+import Quantity from "../valueObjects/Quantity.ts";
+ import { v4 as uuidv4 } from "uuid";
+ 
+ interface CartItemProps {
+   productId: string;
+   productName: string;
+   quantity: Quantity;
+   unitPrice: number;
+   cartItemId?: string;
+ }
+ 
+ class CartItem {
+   public readonly productId: string;
+   public readonly productName: string;
+   public readonly quantity: Quantity;
+   public readonly unitPrice: number;
+   public readonly cartItemId: string;
+ 
+   constructor({
+     productId,
+     productName,
+     quantity,
+     unitPrice,
+     cartItemId,
+   }: CartItemProps) {
+     this.productId = productId;
+     this.productName = productName;
+     this.quantity = quantity;
+     this.unitPrice = unitPrice;
+     this.cartItemId = cartItemId ?? uuidv4();
+     // cartItemId to be used otherwise everytime updateQunatity called, it will regenerate a new one
+   }
+ 
+   public updateQuantity(quantity: Quantity) {
+     return new CartItem({ ...this, quantity });
+   }
+ }
+ 
+ export default CartItem;
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,25 @@
-import Cart from "../shopping-cart-api/src/domain/entities/Cart.js";
-import CartItem from "../shopping-cart-api/src/domain/entities/CartItem.js";
-import Quantity from "../shopping-cart-api/src/domain/valueObjects/Quantity.js";
-
-const cartItem1 = new CartItem({
-  productId: "12345",
-  productName: "test",
-  quantity: new Quantity({ quantity: 1 }),
-  unitPrice: 123,
-});
-
-const cartItem2 = new CartItem({
-  productId: "123",
-  productName: "test",
-  quantity: new Quantity({ quantity: 1 }),
-  unitPrice: 123,
-});
-
-const cart = new Cart({
-  items: [cartItem1, cartItem2],
-});
-const x = cart.addCartItem(cartItem1);
-<<<<<<< HEAD
-const y = cart.clearAllItems()
-console.log("here -", y);
-=======
-const y = cart.addCartItem(cartItem1);
-const z = cart.addCartItem(cartItem1);
-console.log("here -", z);
->>>>>>> parent of 7e9540c (Add calcualteTotalPrice)
+import Cart from "./domain/entities/Cart.ts";
+ import CartItem from "./domain/entities/CartItem.ts";
+ import Quantity from "./domain/valueObjects/Quantity.ts";
+ 
+ const cartItem1 = new CartItem({
+   productId: "12345",
+   productName: "test",
+   quantity: new Quantity({ quantity: 1 }),
+   unitPrice: 123,
+ });
+ 
+ const cartItem2 = new CartItem({
+   productId: "123",
+   productName: "test",
+   quantity: new Quantity({ quantity: 1 }),
+   unitPrice: 123,
+ });
+ 
+ const cart = new Cart({
+   items: [cartItem1, cartItem2],
+ });
+ const x = cart.addCartItem(cartItem1);
+ const y = cart.addCartItem(cartItem1);
+ const z = cart.addCartItem(cartItem1);
+ console.log("here -", z);
